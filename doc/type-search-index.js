3fbf8a9b34573d0a3cdaeaf30329bde27add1856
Lea Jell <lea.jell@hs-augsburg.de>
2020-06-21 17:19:59 +0200

    generated new javadoc

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"flappyBird","l":"Bird"},{"p":"flappyBird","l":"FlappyBirdGame"},{"p":"flappyBird","l":"Pillar"},{"p":"flappyBird","l":"Skull"}]
